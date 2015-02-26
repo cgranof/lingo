b868326203b96d0a9c9bc427a8d39b91850d1db6
schuchard <kbs177@gmail.com>
2015-02-26 07:57:33 -0700

    add click handlers for lang buttons, update styling, animation

--- public/quiz.js
+++ public/quiz.js
@@ -1,26 +1,29 @@
 // var Promise = require('promise');
-
+var lang = '';
 $(document).on('ready', function(){
 
 	$('.langButton').on('click', function(){
 		console.log('click');
-		$('.quiz-container').show();
+		$('.quiz-container').fadeIn();
+		lang = $(this).val();
+		console.log(lang);
+
 
 	});
 
 	$('#quiz-form').on('submit', function(e){
 		e.preventDefault();
 		var word = $('#word').val();
-		var to = $('#to').val();
-		var from = $('#langCode').val();
+		// var to = $('#to').val();
+		// var from = $('#langCode').val();
 
 		var wordObject = {
 			text: word,
-			from: from,
-			to: to
+			from: 'eng',
+			to: lang
 		};
 		console.log(wordObject);
 		console.log('click');
-	
+
 	});
 });
